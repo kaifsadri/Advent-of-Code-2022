0bd7412d60225d0851c4e7a45377ec10a58d1602
Kai F. Sadri <58483176+kaifsadri@users.noreply.github.com>
2022-12-02 08:36:54 -0500

    Add files via upload

--- day02.py
+++ day02.py
@@ -1,25 +1,27 @@
-L = list(line.strip().split() for line in open("day02.txt", "r").readlines())
+L = open("day02.txt", "r").readlines()
 
-
-def Play(player, me):
-    Move = {
-        "X": {"equals": "A", "score": 1, "wins": "C"},
-        "Y": {"equals": "B", "score": 2, "wins": "A"},
-        "Z": {"equals": "C", "score": 3, "wins": "B"},
-    }
-    if Move[me]["wins"] == player:
-        return Move[me]["score"] + 6
-    elif Move[me]["equals"] == player:
-        return Move[me]["score"] + 3
-    else:
-        return Move[me]["score"]
-
-
-print(f"Part 1: {sum(Play(line[0],line[1]) for line in L)}")
-
-Score = {
-    "A": {"X": 3 + 0, "Y": 1 + 3, "Z": 2 + 6},
-    "B": {"X": 1 + 0, "Y": 2 + 3, "Z": 3 + 6},
-    "C": {"X": 2 + 0, "Y": 3 + 3, "Z": 1 + 6},
+Game = {
+    "A X\n": 4,
+    "A Y\n": 8,
+    "A Z\n": 3,
+    "B X\n": 1,
+    "B Y\n": 5,
+    "B Z\n": 9,
+    "C X\n": 7,
+    "C Y\n": 2,
+    "C Z\n": 6,
 }
-print(f"Part 2: {sum(Score[line[0]][line[1]] for line in L)}")
+print(f"Part 1: {sum(Game[line] for line in L)}")
+
+Game = {
+    "A X\n": 3,
+    "A Y\n": 4,
+    "A Z\n": 8,
+    "B X\n": 1,
+    "B Y\n": 5,
+    "B Z\n": 9,
+    "C X\n": 2,
+    "C Y\n": 6,
+    "C Z\n": 7,
+}
+print(f"Part 2: {sum(Game[line] for line in L)}")
